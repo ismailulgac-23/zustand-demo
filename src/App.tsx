b4309847ae82771d51cdb56b8f74773605c311f2
ismailulgac-23 <82471789+ismailulgac-23@users.noreply.github.com>
2022-08-06 01:19:49 +0300

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,14 @@ import Counter from "./Counter";
 import useStore from "./store"
 
 const App = () => {
+  const store = useStore();
   return (
     <div>
       <h1>Zustand React Example</h1>
-      {useStore.getState().counter}
+      {store.counter}
+      <button style={{ marginLeft: 12 }} onClick={() => store.changeCounter(250)}>Change Counter</button>
+      <hr/>
+      <Counter />
     </div>
   )
 }
